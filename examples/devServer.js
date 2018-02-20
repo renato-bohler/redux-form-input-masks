450021631cf1205921bbc60fd934605df4b8e4e4
Renato Böhler <renato.bohler@gmail.com>
2018-02-20 18:40:19 -0300

    chore(docs): changes devServer to allow external access

--- examples/devServer.js
+++ examples/devServer.js
@@ -19,7 +19,7 @@ app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(3030, 'localhost', function(err) {
+app.listen(3030, '0.0.0.0', function(err) {
   if (err) {
     console.log(err);
     return;
