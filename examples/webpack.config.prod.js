0cb6ab86d4d3c40f91561d9a1f2fd2818ee7ff67
Renato Böhler <renato.bohler@gmail.com>
2018-03-09 21:33:55 -0300

    docs: fixes documentation not working on IE (production)

--- examples/webpack.config.prod.js
+++ examples/webpack.config.prod.js
@@ -27,7 +27,7 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        include: path.join(__dirname, 'src'),
+        include: [path.join(__dirname, 'src'), path.join(__dirname, '../src')],
       },
       {
         test: /\.jsx$/,
