f9434268ba18aab0e03c179d5a6d72b42dba56ad
Renato Böhler <renato.bohler@gmail.com>
2018-03-09 23:31:24 -0300

    docs(createNumberMask): creates the `Build your own` section

--- examples/src/CreateTextMask/CreateTextMask.jsx
+++ examples/src/CreateTextMask/CreateTextMask.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
-// import { Field, reduxForm, formValueSelector } from 'redux-form';
-import { Field, reduxForm } from 'redux-form';
+import { Field, reduxForm, formValueSelector, change } from 'redux-form';
 import { createTextMask } from '../../../src/index';
-import { Markdown, Values } from 'redux-form-website-template';
-import { App, Demo } from '../App';
+import { Markdown, Values, Code } from 'redux-form-website-template';
+import { App, Demo, ResultCode } from '../App';
 import documentation from './CreateTextMask.md';
 
-// const selector = formValueSelector('textMask');
+const selector = formValueSelector('textMask');
 
 const guidedStripped = createTextMask({
   pattern: '(999) 999-9999',
@@ -30,6 +29,36 @@ const norGuidedOrStripped = createTextMask({
 });
 
 let CreateTextMask = props => {
+  // createTextMask on try/catch to build custom mask
+  let safeTextMask;
+  let customizedCode;
+  let error;
+  try {
+    safeTextMask = createTextMask({
+      pattern: props.pattern,
+      placeholder: props.placeholder,
+      guide: props.guide,
+      stripMask: props.stripMask,
+    });
+
+    customizedCode =
+      "import { createTextMask } from 'redux-form-input-masks';\n\n" +
+      'const myCustomTextMask = createTextMask({\n' +
+      `    pattern: '${props.pattern}',\n` +
+      (props.placeholder !== '_'
+        ? `    placeholder: '${props.placeholder}',\n`
+        : '') +
+      (props.guide !== true ? `    guide: ${props.guide},\n` : '') +
+      (props.stripMask !== true ? `    stripMask: ${props.stripMask},\n` : '') +
+      '    // maskDefinitions: myCustomMaskDefinitions,\n' +
+      '    // onChange: value => console.log(value),\n' +
+      '    // onCompletePattern: value => console.log(value),\n' +
+      '});';
+  } catch (e) {
+    customizedCode = '// Fix the errors above to generate the code';
+    error = e.message;
+  }
+
   return (
     <App>
       <div className="path">
@@ -115,18 +144,95 @@ let CreateTextMask = props => {
             })}
           />
         </div>
+        <div>
+          <Values form="textMask" />
+        </div>
+        <h2>Build your own</h2>
+        {error && (
+          <div className="buildError">
+            <strong>Error: </strong>
+            {error}
+          </div>
+        )}
+        <div>
+          <h3>Customized</h3>
+          <Field
+            name="customized"
+            component="input"
+            type="tel"
+            {...safeTextMask}
+          />
+        </div>
+        <div>
+          <h3>
+            <small>
+              using <strong>defaultMaskDefinitions</strong> (see above)
+            </small>
+          </h3>
+          <Field
+            name="pattern"
+            component="input"
+            type="text"
+            placeholder="Pattern should not be empty"
+            onChange={() => props.clearCustomValue()}
+          />
+        </div>
+        <div>
+          <div />
+          <Field
+            name="placeholder"
+            component="input"
+            type="text"
+            placeholder="Placeholder should be a char"
+            onChange={() => props.clearCustomValue()}
+          />
+        </div>
+        <div>
+          <div />
+          <label>
+            Show guide
+            <Field
+              name="guide"
+              component="input"
+              type="checkbox"
+              onChange={() => props.clearCustomValue()}
+            />
+          </label>
+        </div>
+        <div>
+          <div />
+          <label>
+            Strip mask
+            <Field
+              name="stripMask"
+              component="input"
+              type="checkbox"
+              onChange={() => props.clearCustomValue()}
+            />
+          </label>
+        </div>
       </form>
-      <Values form="textMask" />
+      <ResultCode />
+      <Code source={customizedCode} />
     </App>
   );
 };
 
-// const mapStateToProps = state => selector(state, 'basic');
-const mapStateToProps = () => ({});
+const mapStateToProps = state =>
+  selector(state, 'pattern', 'placeholder', 'guide', 'stripMask');
 
-CreateTextMask = connect(mapStateToProps)(CreateTextMask);
+const mapDispatchToProps = dispatch => ({
+  clearCustomValue: () => dispatch(change('textMask', 'customized', '')),
+});
+
+CreateTextMask = connect(mapStateToProps, mapDispatchToProps)(CreateTextMask);
 
 export default reduxForm({
   form: 'textMask',
-  initialValues: {},
+  initialValues: {
+    pattern: '(999) 999-9999',
+    placeholder: '_',
+    guide: true,
+    stripMask: true,
+  },
 })(CreateTextMask);
